75921acb8fd9b480eec74396486ceb343cd86d9d
Daurelian <90376706+Daurelian@users.noreply.github.com>
2021-10-13 12:23:12 +0200

    scheletro base

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 function App() {
   return (
-    <div className='container'>
-      <div className='row'>
-        <div className='col-4'>
+    <div className="container">
+      <div className="row">
+        <div className="col-4">
+          {/* Filtro */}
+          <p>Testo Filtro</p>
+        </div>
+        <div className="col-8">
+          <div className="row">
+            {/* Card */}
+            <div className="col-4">
+              <i class="fas fa-heart"></i>
+            </div>
+            <div className="col-4">
+              <i class="fas fa-star"></i>
+            </div>
+            <div className="col-4">
+              <p>Test font Avenir</p>
+              <h3>Test font Avenir Black</h3>
+            </div>
+          </div>
+        </div>
+        {/* <div className='col-4'>
           <i class='fas fa-heart'></i>
         </div>
         <div className='col-4'>
@@ -11,7 +30,7 @@ function App() {
         <div className='col-4'>
           <p>Test font Avenir</p>
           <h3>Test font Avenir Black</h3>
-        </div>
+        </div> */}
       </div>
     </div>
   );
