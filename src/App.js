673c35f684556770174253d87f0073f657973bbd
YaevinnKohai <ornellapatti11@outlook.it>
2021-10-13 18:02:11 +0200

    Filter Fixed

--- src/App.js
+++ src/App.js
@@ -5,35 +5,6 @@ function App() {
       <div className="row">
         <div className="col-4">
           {/* Filtro */}
-          <label>CATEGORIE</label>
-          <p>Riscaldamento</p>
-          <ol>
-            <p> Casa e acqua </p>
-            <p> Casa </p>
-            <p> Acqua </p>
-          </ol>
-          <hr size="2"></hr>
-          <div>
-            <label>SCEGLI PER</label>
-            <p> Disponibili </p>
-            <p> In promozione </p>
-            <label>MARCHE</label>
-            <p> Ariston </p>
-            <p> Baxi </p>
-            <p> Ariston </p>
-            <label>TIPOLOGIA</label>
-            <p> A Condensazione </p>
-            <p> Camera Aperta </p>
-            <p> Camera Stagna </p>
-            <label>ALIMENTAZIONE</label>
-            <p> Elettrica </p>
-            <p> Metano </p>
-            <label>POTENZA NOMINALE(kW)</label>
-            <p> 19.5 </p>
-            <p> 24 </p>
-            <p> 25 </p>
-            <p> da </p>
-          </div>
           <p class="filterTitle">CATEGORIE</p>
           <h5 class="filterSubTitle">Riscaldamento</h5>
             <ol>
