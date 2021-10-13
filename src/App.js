57fc8351d2ddad7fbe1d22e8fe59fab2421fd4e9
YaevinnKohai <ornellapatti11@outlook.it>
2021-10-13 14:28:53 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,8 +14,29 @@ function App() {
             <p> Acqua </p>
             </ol>
           </p>
+          <hr size="3"></hr>
+          <div>
+            <label>SCEGLI PER</label>
+            <p> Disponibili </p>
+            <p> In promozione </p> 
+            <label>MARCHE</label>
+            <p> Ariston </p>
+            <p> Baxi </p> 
+            <p> Ariston </p>
+            <label>TIPOLOGIA</label>
+            <p> A Condensazione </p>
+            <p> Camera Aperta </p> 
+            <p> Camera Stagna </p>
+            <label>ALIMENTAZIONE</label>
+            <p> Elettrica </p>
+            <p> Metano </p> 
+            <label>POTENZA NOMINALE(kW)</label>
+            <p> 19.5 </p>
+            <p> 24 </p> 
+            <p> 25 </p> 
+            <p> da </p> 
+</div>s
           </div>
-          
         </div>
         <div className="col-8">
           <div className="row">
@@ -42,9 +63,11 @@ function App() {
           <p>Test font Avenir</p>
           <h3>Test font Avenir Black</h3>
         </div> */}
-      </div>
-    </div>
+
   );
 }
 
 export default App;
+
+
+
