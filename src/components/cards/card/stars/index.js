e7c2be79688c706251cce0421d53f507a3d247bd
Daurelian <90376706+Daurelian@users.noreply.github.com>
2021-10-14 23:39:26 +0200

    commit di mezzanotte, sostituzione swith con map

--- src/components/cards/card/stars/index.js
+++ src/components/cards/card/stars/index.js
@@ -1,56 +1,63 @@
 import React from "react";
 
 export default function Stars({ stelle }) {
-  switch (stelle) {
-    case 1:
-      return (
-        <>
-          <i class="fas fa-star icon_product"></i>
-          <i class="far fa-star icon_product"></i>
-          <i class="far fa-star icon_product"></i>
-          <i class="far fa-star icon_product"></i>
-          <i class="far fa-star icon_product"></i>
-        </>
-      );
-    case 2:
-      return (
-        <>
-          <i class="fas fa-star icon_product"></i>
-          <i class="fas fa-star icon_product"></i>
-          <i class="far fa-star icon_product"></i>
-          <i class="far fa-star icon_product"></i>
-          <i class="far fa-star icon_product"></i>
-        </>
-      );
-    case 3:
-      return (
-        <>
-          <i class="fas fa-star icon_product"></i>
-          <i class="fas fa-star icon_product"></i>
-          <i class="fas fa-star icon_product"></i>
-          <i class="far fa-star icon_product"></i>
-          <i class="far fa-star icon_product"></i>
-        </>
-      );
-    case 4:
-      return (
-        <>
-          <i class="fas fa-star icon_product"></i>
-          <i class="fas fa-star icon_product"></i>
-          <i class="fas fa-star icon_product"></i>
-          <i class="fas fa-star icon_product"></i>
-          <i class="far fa-star icon_product"></i>
-        </>
-      );
-    case 5:
-      return (
-        <>
-          <i class="fas fa-star icon_product"></i>
-          <i class="fas fa-star icon_product"></i>
-          <i class="fas fa-star icon_product"></i>
-          <i class="fas fa-star icon_product"></i>
-          <i class="fas fa-star icon_product"></i>
-        </>
-      );
-  }
+  return [...Array(5)].map((el, index) => {
+    if (index < stelle) {
+      return <i key={index} class="fas fa-star icon_product"></i>;
+    }
+    return <i key={index} class="far fa-star icon_product"></i>;
+  });
+
+  // switch (stelle) {
+  //   case 1:
+  //     return (
+  //       <>
+  //         <i class="fas fa-star icon_product"></i>
+  //         <i class="far fa-star icon_product"></i>
+  //         <i class="far fa-star icon_product"></i>
+  //         <i class="far fa-star icon_product"></i>
+  //         <i class="far fa-star icon_product"></i>
+  //       </>
+  //     );
+  //   case 2:
+  //     return (
+  //       <>
+  //         <i class="fas fa-star icon_product"></i>
+  //         <i class="fas fa-star icon_product"></i>
+  //         <i class="far fa-star icon_product"></i>
+  //         <i class="far fa-star icon_product"></i>
+  //         <i class="far fa-star icon_product"></i>
+  //       </>
+  //     );
+  //   case 3:
+  //     return (
+  //       <>
+  //         <i class="fas fa-star icon_product"></i>
+  //         <i class="fas fa-star icon_product"></i>
+  //         <i class="fas fa-star icon_product"></i>
+  //         <i class="far fa-star icon_product"></i>
+  //         <i class="far fa-star icon_product"></i>
+  //       </>
+  //     );
+  //   case 4:
+  //     return (
+  //       <>
+  //         <i class="fas fa-star icon_product"></i>
+  //         <i class="fas fa-star icon_product"></i>
+  //         <i class="fas fa-star icon_product"></i>
+  //         <i class="fas fa-star icon_product"></i>
+  //         <i class="far fa-star icon_product"></i>
+  //       </>
+  //     );
+  //   case 5:
+  //     return (
+  //       <>
+  //         <i class="fas fa-star icon_product"></i>
+  //         <i class="fas fa-star icon_product"></i>
+  //         <i class="fas fa-star icon_product"></i>
+  //         <i class="fas fa-star icon_product"></i>
+  //         <i class="fas fa-star icon_product"></i>
+  //       </>
+  //     );
+  // }
 }
