bc48177816c0cddb7c6805625988564ec9903296
YaevinnKohai <ornellapatti11@outlook.it>
2021-10-14 17:34:38 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import Filter from "./components/filter";
 import Cards from "./components/cards";
 
 function App() {
-
-  const [boiler, setBoiler] = useState([])
+  const [boiler, setBoiler] = useState([]);
 
   return (
     <div className="container">
@@ -16,12 +15,40 @@ function App() {
       </div>
       <div className="row">
         <Filter />
-        
+
         <div className="col-9 product">
-         <Cards boiler={boiler} setBoiler={setBoiler} />
-         </div>
+          <Cards boiler={boiler} setBoiler={setBoiler} />
+          <div className="col-12">
+            <hr class="divisor_footer"></hr>
+          </div>
+          <div className="row footer">
+            <div className="col-4">
+              <span className="page_prev">
+                <i class="fas fa-angle-left page_icon_prev"></i>Prev
+              </span>
+            </div>
+            <div className="col-4">
+              <ul className="ul_footer">
+                <a class="active list_link" href="#home">
+                  <li className="li_footer">1</li>
+                </a>
+                <a class="active list_link" href="#home">
+                  <li className="li_footer">2</li>
+                </a>
+                <a class="active list_link" href="#home">
+                  <li className="li_footer">3</li>
+                </a>
+              </ul>
+            </div>
+            <div className="col-4">
+              <span className="page_next">
+                Next<i class="fas fa-angle-right page_icon_next"></i>
+              </span>
+            </div>
+          </div>
         </div>
-       </div>
+      </div>
+    </div>
   );
 }
 
