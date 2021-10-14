0bacd7de44dc917e72d29e82f71f627d4a6a22ca
YaevinnKohai <ornellapatti11@outlook.it>
2021-10-14 17:25:22 +0200

    Update index.js

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -1,28 +1,38 @@
-import React, { useEffect }from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Card from "./card"
+import Card from "./card";
 
-export default function Cards(props) {
-    
-    useEffect(() => {
-        async function fetchData() {
-            const response = await axios.get("http://localhost:9000/boilers")
-            if (response.status === 200) {
-                console.log(response)
-            }
-        }
-        fetchData()
-    }, [])
-    
-    
-return (
-<div className="row">
-            {/* Card */}
-            <div className="col-4 card_product">
-              <Card/>
-            </div>
-            {/* Fine Card */}
-            </div>
-)
-
+export default function Cards({ boiler, setBoiler }) {
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get("http://localhost:9000/boilers");
+      if (response.status === 200) {
+        console.log(response);
+        setBoiler(response.data);
+      }
+    }
+    fetchData();
+  }, []);
+  console.log(boiler);
+  return (
+    <div className="row">
+      {/* Card */}
+      {boiler.map((boiler) => (
+        <div className="col-4 card_product">
+          <Card
+            id={boiler._id}
+            brand={boiler.brand}
+            descrizione={boiler.descrizione}
+            immagine={boiler.immagine}
+            inSconto={boiler.inSconto}
+            note={boiler.note}
+            prezzo={boiler.prezzo}
+            prezzoPreSconto={boiler.prezzoPreSconto}
+            stelle={boiler.stelle}
+          />
+        </div>
+      ))}
+      {/* Fine Card */}
+    </div>
+  );
 }
