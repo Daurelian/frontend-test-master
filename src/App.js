833e24d3a0be2b5a87cdc9b9d93d14502a14fbb1
YaevinnKohai <ornellapatti11@outlook.it>
2021-10-15 15:17:03 +0200

    Update Confront numbercase

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
         </div>
       </div>
       <div className="row fixed_banner">
-        <Banner />
+        <Banner state={state} />
       </div>
     </div>
   );
--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -1,16 +1,59 @@
 import React from "react";
 
-export default function Banner() {
-  return (
-    <>
-      <div className="col-6 banner">
-        <h6 className="text_confront">Seleziona altri due prodotti</h6>
-      </div>
-      <div className="col-6 banner">
-        <button type="button" className="button_confront">
-          CONFRONTA
-        </button>
-      </div>
-    </>
-  );
+export default function Banner({ state }) {
+  if (state.product.length === 0) return "";
+  else if (state.product.length === 1)
+    return (
+      <>
+        <div className="col-6 banner">
+          <h6 className="text_confront">Seleziona altri due prodotti</h6>
+        </div>
+        <div className="col-6 banner">
+          <button type="button" className="button_confront">
+            CONFRONTA
+          </button>
+        </div>
+      </>
+    );
+  else if (state.product.length === 2)
+    return (
+      <>
+        <div className="col-6 banner">
+          <h6 className="text_confront">Seleziona un'altro prodotto</h6>
+        </div>
+        <div className="col-6 banner">
+          <button type="button" className="button_confront">
+            CONFRONTA
+          </button>
+        </div>
+      </>
+    );
+  else if (state.product.length === 3)
+    return (
+      <>
+        <div className="col-6 banner">
+          <h6 className="text_confront">Può essere fatto il confronto</h6>
+        </div>
+        <div className="col-6 banner">
+          <button type="button" className="button_confront">
+            CONFRONTA
+          </button>
+        </div>
+      </>
+    );
+  else if (state.product.length > 3)
+    return (
+      <>
+        <div className="col-6 banner">
+          <h6 className="text_confront">
+            Hai selezionato troppi prodotti, selezionane solo 3
+          </h6>
+        </div>
+        <div className="col-6 banner">
+          <button type="button" className="button_confront">
+            CONFRONTA
+          </button>
+        </div>
+      </>
+    );
 }
